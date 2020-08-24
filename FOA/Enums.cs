8adb1329205f2ce6910433c6e32b905cd25621cc
Gavin White <horace5413@gmail.com>
2020-08-24 14:26:35 -0700

    beginning of material system

---
+++ FOA/Enums.cs
@@ -0,0 +1,12 @@
+﻿namespace FOA {
+    enum MaterialProperties {
+        Flammable,
+        Porous,
+        Meltable,
+        Brittle,
+        Freezable,
+        Dissolvable,
+        Malleable,
+        Conductive
+    }
+}
---
+++ FOA/MaterialProfile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using static FOA.MaterialProperties;
+
+namespace FOA {
+    /// <summary>
+    /// A preset for a material.
+    /// </summary>
+    class MaterialProfile {
+        public Dictionary<MaterialProperties, float> DefaultValues { get; }
+
+        MaterialProfile(params (MaterialProperties property, float amount)[] values) {
+            DefaultValues = new Dictionary<MaterialProperties, float>();
+
+            foreach (var prop in values)
+                DefaultValues[prop.property] = prop.amount;
+        }
+
+        #region Profiles
+
+        public static MaterialProfile Wood = new MaterialProfile(
+            (Flammable, 10),
+            (Porous, 2));
+
+        #endregion
+    }
+}
