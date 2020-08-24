5fb95a5f7bb52927596b4e9e01f71425087d1546
Gavin White <horace5413@gmail.com>
2020-08-23 21:19:21 -0700

    Change FOA project to target .NET Core

---
+++ FOA/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Otter.Core;
+
+namespace FOA {
+    class Program {
+        static void Main() {
+            Game game = new Game("Forest of Ages", 1400, 800) {
+                MouseVisible = true
+            };
+
+            game.Start(new Scenes.TestScene());
+            game.Dispose();
+        }
+    }
+}
---
+++ FOA/Scenes/TestScene.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Otter.Core;
+
+namespace FOA.Scenes {
+    class TestScene : Scene {
+    }
+}
