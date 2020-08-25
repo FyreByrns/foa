88cf8dd1488897b98d60cfeee618ba32a5f8858c
Gavin White <horace5413@gmail.com>
2020-08-25 14:42:43 -0700

    add tie component, which allows tying two entities together

---
+++ FOA/Components/Tie.cs
@@ -0,0 +1,25 @@
+﻿using Otter.Components;
+using Otter.Core;
+
+namespace FOA.Components {
+    /// <summary>
+    /// Ties one entity to another, with an offset.
+    /// </summary>
+    class Tie : Component {
+        public Entity TiedTo { get; }
+        public float OffsetX { get; set; }
+        public float OffsetY { get; set; }
+
+        public override void Update() {
+            base.Update();
+            Entity.X = TiedTo.X + OffsetX;
+            Entity.Y = TiedTo.Y + OffsetY;
+        }
+
+        public Tie(Entity tiedTo, float offsetX = 0, float offsetY = 0) {
+            TiedTo = tiedTo;
+            OffsetX = offsetX;
+            OffsetY = offsetY;
+        }
+    }
+}
