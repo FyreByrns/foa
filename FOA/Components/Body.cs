f537699944756217d3740176460e8802f4264c93
Gavin White <horace5413@gmail.com>
2020-08-29 19:13:08 -0700

    Beginning of body system

---
+++ FOA/Components/Body.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Otter.Components;
+using Otter.Core;
+
+namespace FOA.Components {
+    class Body : Component {
+        public BodyPart MainPart { get; set; }
+
+        public override void Added() {
+            base.Added();
+            if (Entity is BodyPart)
+                MainPart = (BodyPart)Entity;
+        }
+
+        public Body() {
+
+        }
+
+        public class BodyPart : SystemicEntity {
+            public BodyPart Parent { get; set; }
+            public HashSet<BodyPart> Children { get; }
+
+            public BodyPart(BodyPart parent) {
+                Parent = parent;
+                Children = new HashSet<BodyPart>();
+            }
+        }
+    }
+}
---
+++ FOA/SoundManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Otter.Core;
+
+namespace FOA {
+    /// <summary>
+    /// Responsible for loading, unloading, and playing sounds.
+    /// </summary>
+    public static class SoundManager {
+        public static Dictionary<string, SubtitledSound> Sounds { get; } = new Dictionary<string, SubtitledSound>();
+
+        /// <summary>
+        /// Load a sound from a file and assign it a name.
+        /// </summary>
+        public static void LoadSound(string filePath, string name, string subtitle) {
+            Sounds[name] = new SubtitledSound(new Sound(filePath), subtitle);
+        }
+
+        public class SubtitledSound {
+            public Sound Sound { get; }
+            public string Subtitle { get; }
+
+            public SubtitledSound(Sound sound, string subtitle) {
+                Sound = sound;
+                Subtitle = subtitle;
+            }
+        }
+    }
+}
