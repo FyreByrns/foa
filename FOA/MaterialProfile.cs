3968e2f06717221307d566988937e0be1fd85818
Gavin White <horace5413@gmail.com>
2020-08-31 12:57:57 -0700

    allow inheritance of material profiles, add flesh, bone, and flesh with bone profiles.

--- FOA/MaterialProfile.cs
+++ FOA/MaterialProfile.cs
@@ -6,6 +6,17 @@ namespace FOA {
     class MaterialProfile {
         public HashSet<SystemicProperty> Properties { get; }
 
+        /// <summary>
+        /// MaterialProfile with its own declared properties and all the properties of parent profiles.
+        /// </summary>
+        public MaterialProfile InheritingFrom(params MaterialProfile[] parents) {
+            foreach (MaterialProfile parent in parents)
+                foreach (SystemicProperty inheritingProperty in parent.Properties)
+                    Properties.Add(inheritingProperty);
+
+            return this;
+        }
+
         MaterialProfile() {
             Properties = new HashSet<SystemicProperty>();
         }
@@ -15,7 +26,23 @@ namespace FOA {
 
         #region     Profiles
 
-        public static MaterialProfile Wood = new MaterialProfile("Aflame:Temperature above Flashpoint and Dry", "Wet:Moisture above Soakpoint", "Dry:Moisture below Drypoint");
+        public static MaterialProfile Wood = new MaterialProfile(
+            "Aflame:Temperature above Flashpoint and Dry",
+            "Wet:Moisture above Soakpoint",
+            "Dry:Moisture below Drypoint"
+            );
+
+        public static MaterialProfile Flesh = new MaterialProfile(
+            "Burnt:HeatDamage above BurnThreshold",
+            "Cooked:HeatDamage above CookingThreshold"
+            );
+
+        public static MaterialProfile Bone = new MaterialProfile(
+            "Broken:BluntDamage above BoneStrength"
+            );
+
+        public static MaterialProfile FleshWithBones = new MaterialProfile(
+            ).InheritingFrom(Flesh, Bone);
 
         #endregion  Profiles
     }
