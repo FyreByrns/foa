19ae12f9b573268e798c128844d93690cd34953f
Gavin White <horace5413@gmail.com>
2020-08-31 12:58:36 -0700

    allow getting all associated items from an inventory component

--- FOA/Components/Inventory.cs
+++ FOA/Components/Inventory.cs
@@ -1,10 +1,17 @@
 using Otter.Components;
+using System.Collections.Generic;
 
 namespace FOA.Components {
     /// <summary>
     /// Component attached to entities which have inventories.
     /// </summary>
     class Inventory : Component {
-
+        /// <summary>
+        /// Get the items in this inventory.
+        /// </summary>
+        public IEnumerable<Item> GetItems() {
+            foreach (Item item in Entity.GetComponents<Item>())
+                yield return item;
+        }
     }
 }
