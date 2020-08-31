562fa0e18635f97ee081ebf65aeb83ff7245fcf5
Gavin White <horace5413@gmail.com>
2020-08-31 12:59:05 -0700

    playing around with test scene

--- FOA/Scenes/TestScene.cs
+++ FOA/Scenes/TestScene.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using FOA.Components;
 using Otter.Core;
 
 namespace FOA.Scenes {
     class TestScene : Scene {
 
         public TestScene() {
-            Entity player = new Entity();
+            SystemicEntity player = new SystemicEntity();
+            player.AddComponent(Body.MakeHumanoid(fingers: 5, toes: 5, profile: MaterialProfile.FleshWithBones));
+
             player.AddGraphic(Otter.Graphics.Drawables.Image.CreateRectangle(40, 40, Otter.Graphics.Color.White));
             player.Graphic.CenterOrigin();
-            player.AddComponent<Components.PlayerController>();
+            player.AddComponent<PlayerController>();
             Add(player);
         }
     }
